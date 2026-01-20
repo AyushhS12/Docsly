afbe25ceebecfc5d52170a85b28c6e6f98da5037
SourishBeast7 <aayushpal.singh12@gmail.com>
2026-01-20 23:47:05 +0530

    fixed the api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({baseURL:"/api",withCredentials:true})
+const url = import.meta.env.VITE_BACKEND_URL==="http://localhost:7878"?"/api":import.meta.env.VITE_BACKEND_URL
+
+const api = axios.create({baseURL:url,withCredentials:true})
 
 export default api 
